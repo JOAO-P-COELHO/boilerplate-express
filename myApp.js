42379a12afc95557381261e9c758a526a38dc08a
JOAO-P-COELHO <143346573+JOAO-P-COELHO@users.noreply.github.com>
2024-02-22 01:23:11 +0000

    .

--- myApp.js
+++ myApp.js
@@ -4,7 +4,14 @@ let app = express();
 console.log("Hello World")
 
 app.get("/", function(req, res) {
-    res.send('Hello Express');
+    // res.send('Oki, doki, me getting it');
+    absolutePath = __dirname + '/views/index.html'
+    res.sendFile(absolutePath)
+
+    asd = __dirname + "/public"
+
+
+    app.use("/public", express.static(asd))
   })
 
 
