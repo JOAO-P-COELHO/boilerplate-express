4e5a787ceb6ec1e681caafc612cf35f7f546a20d
JOAO-P-COELHO <143346573+JOAO-P-COELHO@users.noreply.github.com>
2024-02-23 21:09:49 +0000

    .

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let app = express();
+require('dotenv').config()
 
 console.log("Hello World");
 
@@ -13,7 +14,23 @@ console.log("Hello World");
 //     res.json("your-app-url/json", {"message": "Hello json"})
 //   })
 
+
+
 app.get("/json", function (req, res){
-  res.json({"message": "Hello json"})});
+  if(process.env.MESSAGE_STYLE === "uppercase"){
+
+  
+    res.json({"message": "HELLO JSON"})}
+    
+  else {res.json({"message": "Hello json"});}
+
+
+
+    
+
+})
+
+  
+
 
 module.exports = app;
