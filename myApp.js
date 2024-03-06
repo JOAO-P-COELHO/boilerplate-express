c6740a3ada600de4d6129da5f1676c46b58ae04d
JOAO-P-COELHO <143346573+JOAO-P-COELHO@users.noreply.github.com>
2024-03-06 00:12:47 +0000

    .

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 let express = require("express");
 let app = express();
-require('dotenv').config()
+// require('dotenv').config()
 
-console.log("Hello World");
+// console.log("Hello World");
 
 // app.get("/", function(req, res) {
 //     // res.send('Oki, doki, me getting it');
@@ -12,32 +12,44 @@ console.log("Hello World");
 //     res.json("your-app-url/json", {"message": "Hello json"})
 //   })
 
-
-
 // app.get("/json", function (req, res){
 //   if(process.env.MESSAGE_STYLE === "uppercase"){
 //     res.json({"message": "HELLO JSON"})}
-  
+
 //   else {res.json({"message": "Hello json"});}
 
 // })
-
 
 // app.use ("/", function (req, res, next){
 // console.log(`${req.method} ${req.path} - ${req.ip}`);
 // next();
 // })
 
-
 // app.get ("/now", function(req, res, next) {
 //   req.time = new Date().toString()
 //   next();
 // }, function(req, res) {
 //   res.json({time: req.time})
-  
+
 // });
 
+// app.get(
+//   "/now",
+//   function (req, res, next) {
+//     req.time = new Date().toString();
+//     next();
+//   },
+//   function (req, res) {
+//     res.send({ time: req.time });
+//   }
+// );
 
+app.get(
+  "/:word/echo",
+  function (req, res, next) {
+    const {word} = req.params;
+    res.json({echo: word});}
+);
 
 
 
